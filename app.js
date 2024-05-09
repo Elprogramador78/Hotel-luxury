869d247ec9b14d917312a3355b24375936820b6f
Cesar Baracaldo <cabaracaldor@gmail.com>
2024-05-09 09:19:31 -0500

    Se agrego el logo y se creo el menu git push

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const menu = document.querySelector(".menu");
-const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn = document.querySelector(".close-menu");
+const toggleMenu = () => {
+    const navigation = document.querySelector(".navigation");
 
+    const burgerMenu = document.querySelector(".menu-icon");
+    const src = burgerMenu.getAttribute("src");
+    
+    const isBurger = src === "images/menu_hamburguer 50.png";
+    const iconName = isBurger ? "images/icons8-multiplicar-50.png":
+        "images/menu_hamburguer 50.png";
 
-function toggleMenu(){
-    menu.classList.toggle("menu_opened");
+    burgerMenu.setAttribute("src", iconName);
+
+    if (!isBurger) {
+        navigation.classList.add
+        ("navigation--mobile--fadeout");
+        setTimeout(() => {
+            navigation.classList.toggle("navigation--mobile");
+
+        }, 300);
+
+    }else{
+        navigation.classList.remove
+        ("navigation--mobile--fadeout");
+        navigation.classList.toggle("navigation--mobile");
+    }
 }
 
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+
+
